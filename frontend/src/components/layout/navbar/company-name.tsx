7e2117428b6ffbd4552396be5309088f442aed3a
James Prentice <jamesprenticez@gmail.com>
2024-10-27 13:10:37 +1300

    fix more build isssues

--- frontend/src/components/layout/navbar/company-name.tsx
+++ frontend/src/components/layout/navbar/company-name.tsx
@@ -2,7 +2,7 @@ import type { ReactElement } from "react";
 import { NavLink } from "react-router-dom";
 import { Path } from "@models/paths";
 
-export const CompanyName = (): ReactElement => {
+export const CompanyName = () => {
   return (
     <NavLink to={Path.HOME}>
       <h1 className="cursor-pointer text-[20px] ml-4 text-white font-[600]">
